a55ade232771921d314e5fc1816443d06268e617
Brian Davidson <briandavidson@brainjocks.com>
2016-11-02 10:51:12 -0400

    Updated config.fish
    added linking to local config for rdp

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,5 @@
 alias rdp "xfreerdp +compression +clipboard +fonts /home-drive /cert-ignore /size:1400x1050"
+
+if test -f ~/.config/fish/config_local.fish
+	source ~/.config/fish/config_local.fish
+end
